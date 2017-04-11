0557779e060a9b33ce746cac6bf559b3eb933278
sdlovecraft <sam@samhains.com>
2017-04-11 08:16:30 -0400

    make some changes to blur and crop

--- crop_tools.py
+++ crop_tools.py
@@ -3,10 +3,11 @@ import utils
 import matplotlib.pyplot as plt
 from skimage.transform import resize
 from multiprocessing.dummy import Pool as ThreadPool 
+from skimage.filters import gaussian
 import numpy as np
 import scipy
 
-NUM_OF_THREADS=100
+NUM_OF_THREADS=1
 
 pool = ThreadPool(NUM_OF_THREADS) 
 dirname = './images'
@@ -22,12 +23,18 @@ filenames_split = np.array_split(filenames, NUM_OF_THREADS)
 # Then resize the square image to 100 x 100 pixels
 def save_cropped(filenames_arr):
     for fname in filenames_arr:
+        print(fname)
         img = scipy.misc.imread(fname)
-        print(img.shape)
         fname = fname.split('/')[-1]
         if(img.shape[0] > 256 and img.shape[1] > 256):
             img = utils.imcrop_tosquare(img)
             img = resize(img, (256, 256))
-            fname = './cropped/'+fname
-            scypi.misc.imsave(fname, img)
+            a_fname = './cropped/'+fname
+            b_fname = './blurred/'+fname
+            b_img = gaussian(img, sigma=12, multichannel=True)
+            scipy.misc.imsave(a_fname, img)
+            scipy.misc.imsave(b_fname, b_img)
 
+
+pool.map(save_cropped, filenames_split)
+#save_cropped(filenames_split)
