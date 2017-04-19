1365a81208e8fa698ac54183ed5e866dc94899e7
sam hains <sam@samhains.com>
2017-04-19 22:00:36 +0200

    use m for one hot encoder

--- one_hot_encoder_tool.py
+++ one_hot_encoder_tool.py
@@ -1,7 +1,8 @@
-import pickle
+import random
 import os
 import scipy.misc
 import numpy as np
+import h5py
 
 
 def get_filenames(dirname):
@@ -12,28 +13,28 @@ def get_filenames(dirname):
 def get_directories(dirname):
     return [x[0] for x in os.walk(dirname)][1:]
 
-def create_one_hot_data(d):
+
+def get_one_hot_data(d):
     dirs = get_directories(d)
 
     images = []
     one_hot_arr_total = []
     for i, dirname in enumerate(dirs):
+        print(i)
 
         one_hot_arr = np.zeros(len(dirs))
         one_hot_arr[i] = 1
 
         filenames = get_filenames(dirname)
+        random.shuffle(filenames)
         for fname in filenames:
             img = scipy.misc.imread(fname)
             images.append(img)
             one_hot_arr_total.append(one_hot_arr)
 
-    return [np.array(images), np.array(one_hot_arr_total)]
+    return np.array(images), np.array(one_hot_arr_total)
 
-
-with open('data.pickle', 'wb') as f:
-    data = create_one_hot_data('./')
-    pickle.dump(data, f)
-
-# with open('data.pickle', 'rb') as f:
-     # data = pickle.load(f)
+with h5py.File('mnist.h5', 'w') as hf:
+    X, Y = get_one_hot_data('./')
+    hf.create_dataset("mnist_X",  data=X)
+    hf.create_dataset("mnist_Y",  data=Y)
