667d6c98b9ff42743cd4dd9dbffdc8cc64464a85
sam hains <sam@samhains.com>
2017-05-10 14:42:34 +1000

    add constant for name

--- prepare_video.py
+++ prepare_video.py
@@ -4,17 +4,19 @@ import utils
 import numpy as np
 from skimage.transform import resize
 
-vidcap = cv2.VideoCapture('1.mp4')
-success,image = vidcap.read()
-count = 0
-success = True
-if not os.path.exists('video_frames'):
-    os.mkdir('video_frames')
-while success:
-  success, image = vidcap.read()
-  img = np.array(image)
-  img = utils.imcrop_tosquare(image)
-  img = resize(img, (256, 256), mode="constant")*255
-  print('Read a new frame: ', success)
-  cv2.imwrite("video_frames/frame%d.jpg" % count, img)     # save frame as JPEG file
-  count += 1
+VIDEONAME = '1'
+def vid_cap():
+    vidcap = cv2.VideoCapture(VIDEONAME+'.mp4')
+    success,image = vidcap.read()
+    count = 0
+    success = True
+    if not os.path.exists(VIDEONAME+'_frames'):
+        os.mkdir(VIDEONAME+'_frames')
+    while success:
+      success, image = vidcap.read()
+      img = np.array(image)
+      img = utils.imcrop_tosquare(image)
+      img = resize(img, (256, 256), mode="constant")*255
+      print('Read a new frame: ', success)
+      cv2.imwrite("video_frames/frame%d.jpg" % count, img)     # save frame as JPEG file
+      count += 1
