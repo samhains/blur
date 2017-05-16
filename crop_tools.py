3c641511bd139007b1c05032915a89d7414fb346
Sam Hains <sdh@eml.cc>
2017-05-15 23:24:07 -0400

    made some changes to crop tolz

--- crop_tools.py
+++ crop_tools.py
@@ -1,4 +1,6 @@
 import os
+from PIL import Image, ImageFilter
+import uuid
 import utils
 import matplotlib.pyplot as plt
 from skimage.transform import resize
@@ -8,8 +10,9 @@ import numpy as np
 import scipy
 
 NUM_OF_THREADS = 1
+FINAL_SLICE_SIZE = 512
 
-filename = 'images'
+filename = 'insta_images'
 dirname = './'+filename
 BLUR_DIRNAME = dirname+'_blurred'
 CROP_DIRNAME = dirname+'_cropped'
@@ -23,7 +26,6 @@ filenames = [os.path.join(dirname, fname)
 filenames = np.asarray(filenames)
 filenames_split = np.array_split(filenames, NUM_OF_THREADS)
 
-print(filenames)
 
 if not os.path.exists(BLUR_DIRNAME):
     os.mkdir(BLUR_DIRNAME)
@@ -36,11 +38,19 @@ def save_cropped(filenames_arr):
         img = scipy.misc.imread(fname)
         fname = fname.split('/')[-1]
         img = utils.imcrop_tosquare(img)
-        img = resize(img, (512, 512))
-        b_img = gaussian(img, sigma=11, mode='constant')
-        filename = dirname.split('.')[-1]
-        b_fname = '{}/{}_{}'.format(BLUR_DIRNAME, filename, fname)
-        scipy.misc.imsave(b_fname, b_img)
+        img = resize(img, (FINAL_SLICE_SIZE, FINAL_SLICE_SIZE))
+        img = gaussian(img, sigma=12, mode='constant')*255
+        img = img.astype('uint8') 
+        new_img = Image.new('RGB', (FINAL_SLICE_SIZE*2, FINAL_SLICE_SIZE))
+        img = Image.fromarray(img)
+        new_img.paste(img, (0, 0))
+        # new_img.paste(img)
+        img = new_img
+        # filename = dirname.split('.')[-1]
+        b_fname = '{}/_{}.png'.format(BLUR_DIRNAME, uuid.uuid4())
+        print(b_fname)
+        print(b_fname)
+        scipy.misc.imsave(b_fname, img)
         # a_fname = '{}/{}_{}'.format(CROP_DIRNAME, filename, fname)
         # print('a_filename', a_fname)
         # scipy.misc.imsave(a_fname, img)
